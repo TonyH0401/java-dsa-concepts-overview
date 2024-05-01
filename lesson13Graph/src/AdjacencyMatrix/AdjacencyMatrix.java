b285fd508ab45863e954a334b6b170f6ac26ce3f
Lê Gia Phú <phugiale2912@gmail.com>
2024-05-01 13:19:34 +0700

    feat: complete adj matrix

--- lesson13Graph/src/AdjacencyMatrix/AdjacencyMatrix.java
+++ lesson13Graph/src/AdjacencyMatrix/AdjacencyMatrix.java
@@ -1,9 +1,13 @@
 package AdjacencyMatrix;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 import GraphInterfaces.*;
 
-public class AdjacencyMatrix implements GraphInterface {
+public class AdjacencyMatrix implements GraphInterface, GraphTraverse {
     private int size;
     private int edges;
     private int[][] adj;
@@ -36,38 +40,115 @@ public class AdjacencyMatrix implements GraphInterface {
 
     @Override
     public void readFile(String dir) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'readFile'");
+        File file = null;
+        Scanner scan = null;
+        String line = "";
+        int sum = 0;
+        try {
+            file = new File(dir);
+            scan = new Scanner(file);
+            // get the row and column of the matrix
+            this.size = scan.nextInt();
+            // move to the next row
+            scan.nextLine();
+
+            adj = new int[size][size];
+
+            for (int i = 0; i < size; i++) {
+                line = scan.nextLine();
+                String[] numbers = line.split(" ");
+                for (int j = 0; j < size; j++) {
+                    int x = Integer.parseInt(numbers[j]);
+                    sum += x;
+                    adj[i][j] = x;
+                }
+            }
+            this.edges = sum / 2;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.err.println(ex.getMessage());
+        } finally {
+            scan.close();
+        }
     }
 
     @Override
     public int numOfVertex() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'numOfVertex'");
+        return size;
     }
 
     @Override
     public int numOfEdge() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'numOfEdge'");
+        return edges;
     }
 
     @Override
     public void enumerateNeighbor(int u) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'enumerateNeighbor'");
+        if (u < 1 || u > size) {
+            throw new RuntimeException(u + " is not valid");
+        }
+        int row = u - 1;
+        for (int i = 0; i < size; i++) {
+            if (adj[row][i] == 1) {
+                System.out.print((i + 1) + " ");
+            }
+        }
+        System.out.println();
     }
 
     @Override
     public boolean hasEdge(int u, int v) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasEdge'");
+        if (u < 1 || u > size || v < 1 || v > size) {
+            throw new RuntimeException("Invalid Input");
+        }
+        u -= 1;
+        v -= 1;
+        return adj[u][v] == 1;
     }
 
     @Override
     public void print() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'print'");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(adj[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
+
+    // ######################################################################## //
+
+    @Override
+    public void BFS(int s) {
+        // if (s < 1 || s > size)
+        //     throw new RuntimeException("Invalid Input!");
+        // boolean[] visited = new boolean[size];
+        // Queue<Integer> queue = new LinkedList<Integer>();
+
+        // visited[s] = true;
+        // queue.add(s);
+
+        // while (!queue.isEmpty()) {
+        //     int x = queue.poll();
+        //     System.out.print((x + 1) + " ");
+        //     for (int i = 0; i < size; i++) {
+        //         if (adj[x][i] != 0 && visited[i] == false) {
+        //             queue.add(i);
+        //             visited[i] = true;
+        //         }
+        //     }
+        // }
+    }
+
+    @Override
+    public void DFS(int s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'DFS'");
+    }
+
+    // ######################################################################## //
 
 }
--- lesson13Graph/src/AdjacencyMatrix/testMain.java
+++ lesson13Graph/src/AdjacencyMatrix/testMain.java
@@ -1,7 +1,16 @@
 package AdjacencyMatrix;
 
+import GraphInterfaces.*;
+
 public class testMain {
     public static void main(String[] args) throws Exception {
-        
+        GraphInterface adjMatrix = new AdjacencyMatrix();
+        String dir = "src\\AdjacencyMatrix\\AM.txt";
+        adjMatrix.readFile(dir);
+        adjMatrix.print();
+        System.out.println("Vertices: " + adjMatrix.numOfVertex());
+        System.out.println("Edges: " + adjMatrix.numOfEdge());
+        adjMatrix.enumerateNeighbor(2);
+        // ######################################################################## //
     }
 }
